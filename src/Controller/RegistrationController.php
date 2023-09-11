e56fd4a26ca6edda5bc660f60d32b447375db667
jamalTa <taoufiqui.j@hotmail.fr>
2023-09-11 11:11:57 +0200

    modification des sauces etc..

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -79,6 +79,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('app_login', 'Votre adresse e-mail a été vérifiée.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('home');
     }
 }
